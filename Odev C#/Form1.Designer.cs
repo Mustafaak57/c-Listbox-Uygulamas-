699f6dcf2cd0ff005e40e66c014df88fb48bf82e
Mustafa Ak <akmustafa703@gmail.com>
2025-04-21 19:10:16 +0300

    Add files via upload

---
+++ Odev C#/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtİl = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lstİller = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lstİller2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İL:";
+            // 
+            // txtİl
+            // 
+            this.txtİl.Location = new System.Drawing.Point(266, 106);
+            this.txtİl.Name = "txtİl";
+            this.txtİl.Size = new System.Drawing.Size(169, 26);
+            this.txtİl.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(463, 105);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(95, 27);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lstİller
+            // 
+            this.lstİller.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lstİller.FormattingEnabled = true;
+            this.lstİller.ItemHeight = 20;
+            this.lstİller.Location = new System.Drawing.Point(129, 161);
+            this.lstİller.Name = "lstİller";
+            this.lstİller.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstİller.Size = new System.Drawing.Size(191, 204);
+            this.lstİller.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(353, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = ">>";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(353, 275);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 41);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "<<";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lstİller2
+            // 
+            this.lstİller2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lstİller2.FormattingEnabled = true;
+            this.lstİller2.ItemHeight = 20;
+            this.lstİller2.Location = new System.Drawing.Point(495, 161);
+            this.lstİller2.Name = "lstİller2";
+            this.lstİller2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstİller2.Size = new System.Drawing.Size(191, 204);
+            this.lstİller2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstİller2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lstİller);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtİl);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Ödev";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtİl;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox lstİller;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox lstİller2;
+    }
+}
+
---
+++ Odev C#/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            lstİller.Items.Add(txtİl.Text);
+            txtİl.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            List<object> selectedItems = new List<object>();
+            foreach (var item in lstİller.SelectedItems)
+            {
+                
+                if (!lstİller2.Items.Contains(item))
+                {
+                    
+                    lstİller2.Items.Add(item);
+                    
+                }
+                selectedItems.Add(item);
+            }
+            foreach (var item in selectedItems)
+            {
+                lstİller.Items.Remove(item);
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<object> selectedItems = new List<object>();
+
+            foreach (var item in lstİller2.SelectedItems)
+            {
+                if (!lstİller.Items.Contains(item))
+                {
+                    lstİller.Items.Add(item);
+                }
+
+                selectedItems.Add(item);
+            }
+
+            foreach (var item in selectedItems)
+            {
+                lstİller2.Items.Remove(item);
+            }
+        }
+
+    }
+}
